2194f3bb122cf30b431a2685559d354ff92ef16d
Moustafa Jazzar <moustafajazzar@MacBook-Air.local>
2022-01-30 19:46:52 +0300

    finger crossed2

--- prepareFirebaseHosting.js
+++ prepareFirebaseHosting.js
@@ -22,3 +22,14 @@ directories.forEach(({ name }) => {
         }
     });
 })
+
+let mainSourceDirectory = './dist';
+let mainDestinationDirectory = './public';
+
+fse.copySync(mainSourceDirectory, mainDestinationDirectory, { overwrite: true }, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`files copied to ${mainDestinationDirectory}`);
+    }
+});
