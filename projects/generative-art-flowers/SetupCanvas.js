163ea39dfe8c7d5e8a51a2c0aa8f0daf9ef82874
Moustafa Jazzar <moustafajazzar@192.168.1.5>
2022-03-27 21:11:46 +0200

    fix flowers bug

--- projects/generative-art-flowers/SetupCanvas.js
+++ projects/generative-art-flowers/SetupCanvas.js
@@ -1,35 +1,14 @@
-import { ctx } from "./SetupCanvas.js";
-export class Flower {
-    constructor(x, y, size) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.vs = Math.random() * 0.3 + 0.2;
-        this.maxFlowerSize = this.size + Math.random() * 80;
-        this.image = new Image();
-        this.image.src = 'https://www.frankslaboratory.co.uk/downloads/flowers.png';
-        this.frameSize = 100;
-        this.frameX = Math.floor(Math.random() * 3);
-        this.frameY = Math.floor(Math.random() * 3);
-        this.size > 11.5 ? this.willFlower = true : this.willFlower = false;
-        this.angle = 0
-        this.va = Math.random() * 0.05 + 0.025;
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext('2d')
 
-        this.grow()
-    }
+canvas.style.width = `${innerWidth}px`;
+canvas.style.height = `${innerHeight}px`;
 
-    grow = () => {
-        if (this.size < this.maxFlowerSize && this.willFlower) {
-            this.size += this.vs;
-            this.angle += this.va;
+const scale = window.devicePixelRatio * 5;
+canvas.width = innerWidth * scale;
+canvas.height = innerHeight * scale;
 
-            ctx.save();
-            ctx.translate(this.x, this.y);
-            ctx.rotate(this.angle);
-            ctx.drawImage(this.image, this.frameX * this.frameSize, this.frameY * this.frameSize, this.frameSize, this.frameSize, 0 - this.size / 2, 0 - this.size / 2, this.size, this.size);
-            ctx.restore()
+ctx.scale(scale, scale);
 
-            requestAnimationFrame(this.grow.bind(this));
-        }
-    }
-}
+ctx.lineWidth = 0.4;
+export { ctx }
